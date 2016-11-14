b8c0e59624b7cc245c8f3aae46ef934e259e5d04
Sojin Yang <Sojin Yang>
2016-11-14 18:35:48 +0900

    load Event

--- js/event.js
+++ js/event.js
@@ -2,20 +2,14 @@
 function addDiv(date, url, title) {
 
     var list = document.getElementById('EventList');
-    var image = document.createElement("img");
-    image.src = 'calendar.png';
-    list.appendChild(image);
+    var divtest = document.getElementById('temp');
 
+    //divtest.src = './public/resource/event_fireworks.jpg';
     //divtest.style.backgroundImage = "url('calendar.png')";
-        //'<div style="background-image: url(' + url +
-        //+ ');"></div>' // ' //date + " + " + image + " + " + txt';
-    //divtest.
 
-
-    //list.appendChild(divtest);
+    list.appendChild(divtest);
     divtest.onclick = loadEvent;
 }
-
 
 
 function loadEvent(){
@@ -29,14 +23,14 @@ function event_init() {
     var index_events;
     for (index_events = 0; index_events < 3; index_events++) {
         event_array.push(new EventClass(index_events + 1, index_events * 10 + 3, index_events * 100 + 3));
-        addDiv(event_array[index_events].date, event_array[index_events].url, event_array[index_events].title);
+        addDiv(event_array[index_events].url, event_array[index_events].title,event_array[index_events].date);
     }
 }
 
-function EventClass (date, url, title) {
-    this.date = date;
+function EventClass (url, title, date) {
     this.url = url;
     this.title = title;
+    this.date = date;
 }
 
 
