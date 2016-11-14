a17e8e7c807230144f82b2e60f1f72c3e64e8ac5
Sojin Yang <Sojin Yang>
2016-11-14 17:26:36 +0900

    Event list append

--- js/event.js
+++ js/event.js
@@ -1,14 +1,22 @@
 
-function addDiv(image, txt) {
+function addDiv(date, url, title) {
 
     var list = document.getElementById('EventList');
-    var divtest = document.createElement("div");
-    divtest.innerHTML = image + " + " + txt;
+    var image = document.createElement("img");
+    image.src = 'calendar.png';
+    list.appendChild(image);
 
-    list.appendChild(divtest);
+    //divtest.style.backgroundImage = "url('calendar.png')";
+        //'<div style="background-image: url(' + url +
+        //+ ');"></div>' // ' //date + " + " + image + " + " + txt';
+    //divtest.
+
+
+    //list.appendChild(divtest);
     divtest.onclick = loadEvent;
-
 }
+
+
 
 function loadEvent(){
     document.getElementById('EventList').style.display = 'none';
@@ -17,15 +25,20 @@ function loadEvent(){
 
 function event_init() {
     var event_array = new Array();
+
     var index_events;
     for (index_events = 0; index_events < 3; index_events++) {
-        event_array.push(new EventClass(index_events + 1, index_events * 100 + 3));
-        addDiv(event_array[index_events].image, event_array[index_events].txt);
-
+        event_array.push(new EventClass(index_events + 1, index_events * 10 + 3, index_events * 100 + 3));
+        addDiv(event_array[index_events].date, event_array[index_events].url, event_array[index_events].title);
     }
 }
 
-function EventClass (image, txt) {
-    this.image = image;
-    this.txt = txt;
+function EventClass (date, url, title) {
+    this.date = date;
+    this.url = url;
+    this.title = title;
 }
+
+
+
+
