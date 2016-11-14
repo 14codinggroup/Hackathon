b117d322a3061f455d7aca41f97e39bc4c53a5c5
Dujin Jeong <wjdenwls123@gmail.com>
2016-11-14 14:20:53 +0900

    memo complete

--- js/basic_ui.js
+++ js/basic_ui.js
@@ -3,8 +3,7 @@ function onclick_memo(){
     //lobby ui --> memo ui
     document.getElementById('MiddleContent').style.display = 'none';
     document.getElementById('FooterContent').style.display = 'none';
-    document.getElementById('MemoCanvas').style.display = 'block';
-    document.getElementById('MemoBtnContainer').style.display = 'block';
+    document.getElementById('MemoPage').style.display = 'block';
     resize_canvas();
 }
 
