b37e3cb67492471847860c2a8bf4a20123cc8a64
Dujin Jeong <wjdenwls123@gmail.com>
2016-11-17 19:38:17 +0900

    android

--- Android/FirebaseNotificate/app/src/main/java/com/dujin/firebasenotificate/MainActivity.java
+++ Android/FirebaseNotificate/app/src/main/java/com/dujin/firebasenotificate/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -11,11 +13,21 @@ import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    WebView mWebview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        mWebview  = new WebView(this);
+        mWebview.setWebViewClient(new WebViewClient());
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        mWebview .loadUrl("http://121.135.150.5:12111/");
+        setContentView(mWebview );
+
+        /*
         Button btnShowToken = (Button)findViewById(R.id.button_show_token);
         btnShowToken.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -27,5 +39,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         );
+        */
     }
 }
